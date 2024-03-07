5d0e3e712f1cad412f5e0df7866e5496c2ebd486
Gabriel <gabrielstevematamoros@gmail.com>
2024-03-07 04:49:53 -0500

    Primera subida...

---
+++ Metodos2/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos2
+{
+    //ATRIBUTOS:Nombre, nacionalidad.
+    class Persona
+    {
+        private string nombre;
+        private string nacionalidad;
+
+        public Persona( string nombre, string nacionalidad)
+        {
+            this.nombre = nombre;
+            this.nacionalidad = nacionalidad;
+        }
+          public string Nacionalidad
+        {
+            get { return nacionalidad; }
+        }
+
+   //METODOS: SALUDAR
+        public string saludar()
+        {
+            return "Hola soy..." + nombre;
+        }
+  //SOBRE CARGA DE METODO
+       public string saludar(string personaje)
+        {
+            return "Hola " + personaje + ", soy" + nombre;
+        }
+    }
+}
---
+++ Metodos2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metodos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("PEPE" , "Español");
+            Console.WriteLine(p1.saludar());
+            Console.WriteLine(" Y soy " + nacionalidad);
+            Console.WriteLine(p1.saludar("GABO"));
+        }
+
+   
+    }
+}
